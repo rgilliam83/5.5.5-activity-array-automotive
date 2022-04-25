0e8a8d0fa3d6f3e4f04443359e49bf8668fd5b2e
Rashad Gilliam <akklaim83@gmail.com>
2022-04-24 22:31:46 -0700

    changes

--- vehicle.js
+++ vehicle.js
@@ -14,10 +14,8 @@ class Vehicle {
     start() {
         if (this.fuel > 0) {
             return this.started = true;
-            console.log("engine started...!!!");
         } else {
             return this.started = false;
-            console.log("engine cannot start...");
         }
     }
     accelerate() {
